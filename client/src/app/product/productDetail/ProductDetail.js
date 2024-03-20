0d11eb557676db762fe8a39b316f8b17d55b8b48
Tran Thai Duy <ttduycapricorn@gmail.com>
2024-03-20 17:28:14 +0700

    commit -m

--- client/src/app/product/productDetail/ProductDetail.js
+++ client/src/app/product/productDetail/ProductDetail.js
@@ -1,14 +1,9 @@
-import classNames from 'classnames'
-import style from './ProductDetail.module.scss'
+import classNames from 'classnames';
+import style from './ProductDetail.module.scss';
 const cx = classNames.bind(style);
 
 function ProductDetail() {
-
-    return (
-        <div className={cx('detail')}>
-            hello
-        </div>
-    )
+    return <div className={cx('detail')}>hello</div>;
 }
 
 export default ProductDetail;
